9f31d1b4a03e7e78f607b0310444653e630d8d48
Mdsuhail5 <mohammedsuhail55410@gmail.com>
2025-04-15 07:32:58 +0530

    javascrit basics

---
+++ practice.js
@@ -0,0 +1,15 @@
+//Javascript is used to websites along with html,css and is used to add funcitonalities to the website
+// 1. javascript is dynamically typed lang
+// 2. javascript runs on single thread
+// 3. javascript is intrepreted and runtime language
+// 4. javascript has garbage collector which automatically reclaims the memory used by objects and variables
+
+//Syntax of Javascript
+//var, let ,const
+var isStudent = true;
+console.log(isStudent);
+const w = 10
+console.log(w)
+w = 9
+console.log(w)
+
