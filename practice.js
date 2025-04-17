dc424edd0591be2211be8606992cdda7c2754358
Mdsuhail5 <mohammedsuhail55410@gmail.com>
2025-04-17 23:59:53 +0530

    methods practice

--- practice.js
+++ practice.js
@@ -10,5 +10,57 @@ var isStudent = true;
 console.log(isStudent);
 const w = 10
 console.log(w)
-w = 9
+//w = 9
 console.log(w)
+
+
+//Callbacks
+// function taking another funtion as an argument 
+
+function sum(a, b) {
+    let res = 0;
+    res = a + b;
+    return res;
+}
+
+function display(a, b, funtocall) {
+    let result = funtocall(a, b)
+    console.log(result);
+}
+
+display(1, 2, sum)
+
+
+//example 2
+
+function calcarithemetic(a, b, arithmeticfunction) {
+    const ans = arithmeticfunction(a, b);
+    return ans;
+}
+
+function sum(a, b) {
+    return a + b;
+}
+
+
+const value = calcarithemetic(20, 30, sum)
+console.log(value)
+
+
+//example
+//setTimeout(fun,duration)
+function greet() {
+    console.log("Hello World!")
+}
+
+setTimeout(greet, 1 * 1000)
+
+//example 
+//setInterval(fun,duration)
+
+function greetAlien() {
+    console.log("Hello alien!")
+}
+
+
+setInterval(greetAlien, 3 * 1000)
